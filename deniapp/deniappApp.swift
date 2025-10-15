083471e950f29fd20fa519e2d055b413828e208e
Omar Cruz <omar.cruzr97@gmail.com>
2025-10-14 18:33:39 -0600

    Initial Commit

---
+++ deniapp/deniappApp.swift
@@ -0,0 +1,17 @@
+//
+//  deniappApp.swift
+//  deniapp
+//
+//  Created by Omar Cruz on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct deniappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
